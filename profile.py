30f3780486c1108849c0e13333fb9cae146e8ef1
edlet <edlawitmesfin55@gmail.com>
2026-07-09 08:35:44 -1200

    add python program

---
+++ profile.py
@@ -0,0 +1,11 @@
+name= "Edlawit Mesfin"
+Age= 27
+city= "Addis Ababa"
+Occupation= "Business Owner"
+Fav_Hobby= "Reading"
+print ("my name is", name)
+print ("my age is ", Age)
+print ("my occuption is ", Occupation)
+print ("my faviorite hobby is", Fav_Hobby)
+
+
